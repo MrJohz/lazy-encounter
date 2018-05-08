d917e3d57f19cff1056ab984194533d7ccb96cf3
Jonathan <jonathan.frere@gmail.com>
2018-05-08 16:22:28 +0100

    Move shorty tests into describe blocks

--- test/unit/shorty/shorty.spec.ts
+++ test/unit/shorty/shorty.spec.ts
@@ -4,33 +4,37 @@ import expect from 'must';
 
 describe('Shorty', () => {
 
-    it('should return a new shortcut', () => {
-        const shorty = new Shorty();
-        const helloShortcut = shorty.addShortcut('hello');
-        expect(helloShortcut).to.be.instanceOf(ShortcutHandle);
-        expect(helloShortcut.keys).to.eql(['h']);
-    });
+    describe('addShortcut', () => {
 
-    it('shortcuts always made from lc letters', () => {
-        const shorty = new Shorty();
-        const helloShortcut = shorty.addShortcut('HELLO');
-        expect(helloShortcut['name']).to.eql('HELLO');
-        expect(helloShortcut.keys).to.eql(['h']);
-    });
+        it('should return a new shortcut', () => {
+            const shorty = new Shorty();
+            const helloShortcut = shorty.addShortcut('hello');
+            expect(helloShortcut).to.be.instanceOf(ShortcutHandle);
+            expect(helloShortcut.keys).to.eql(['h']);
+        });
 
-    it('shortcuts automatically disambiguate equivalent keys', () => {
-        const shorty = new Shorty();
-        const sc1 = shorty.addShortcut('hello');
-        const sc2 = shorty.addShortcut('hello');
-        const sc3 = shorty.addShortcut('HELLO');
+        it('shortcuts always made from lc letters', () => {
+            const shorty = new Shorty();
+            const helloShortcut = shorty.addShortcut('HELLO');
+            expect(helloShortcut['name']).to.eql('HELLO');
+            expect(helloShortcut.keys).to.eql(['h']);
+        });
 
-        expect(sc1['name']).to.eql('hello');
-        expect(sc1.keys).to.eql(['h', '1']);
+        it('shortcuts automatically disambiguate equivalent keys', () => {
+            const shorty = new Shorty();
+            const sc1 = shorty.addShortcut('hello');
+            const sc2 = shorty.addShortcut('hello');
+            const sc3 = shorty.addShortcut('HELLO');
 
-        expect(sc2['name']).to.eql('hello');
-        expect(sc2.keys).to.eql(['h', '2']);
+            expect(sc1['name']).to.eql('hello');
+            expect(sc1.keys).to.eql(['h', '1']);
 
-        expect(sc3['name']).to.eql('HELLO');
-        expect(sc3.keys).to.eql(['h', '3']);
+            expect(sc2['name']).to.eql('hello');
+            expect(sc2.keys).to.eql(['h', '2']);
+
+            expect(sc3['name']).to.eql('HELLO');
+            expect(sc3.keys).to.eql(['h', '3']);
+        });
+
     });
 });
