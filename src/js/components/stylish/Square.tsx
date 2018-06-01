06d2c916df137c70c62006201309635e85abfb59
Jonathan <jonathan.frere@gmail.com>
2018-06-01 19:40:06 +0100

    Revert "Theming attempt 2"
    This reverts commit be005e5

--- src/js/components/stylish/Square.tsx
+++ src/js/components/stylish/Square.tsx
@@ -2,28 +2,16 @@ import React, { MouseEvent } from 'react';
 import classNames from 'classnames';
 
 import { Children, childrenise, Callback, Optional, ClassName } from '../../utils/jsx-props';
-import { Popup } from './Popup';
 
 import styles from './Square.scss';
 
 type Props = Children
-    & Optional<{ isOpen: boolean }>
-    & Optional<Children<'popup'>>
     & Optional<Callback<'onClick', MouseEvent<HTMLDivElement>>>;
 
 export function Square(props: Props) {
-    const { children, popup, isOpen, ...rest } = props;
+    const { children, ...rest } = props;
 
-    const popupItems = childrenise(popup);
-
-    return <div className={styles.squareBase}>
-        <div className={styles.square} {...rest}>
-            {childrenise(children)}
-            <div className={styles.topShadow}/>
-            <div className={styles.bottomShadow}/>
-        </div>
-        {popupItems && popupItems.length
-            ? <Popup isOpen={isOpen || false}>{popupItems}</Popup>
-            : null }
-    </div>;
+    return <div className={styles.square} {...rest}>{
+        childrenise(children)
+    }</div>;
 }
